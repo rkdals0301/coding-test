a7bc229cd73efc82bda265f5991c3646b37de961
Gangmin Lee <rkdals0301@naver.com>
2023-12-16 03:26:06 +0900

    [level unrated] Title: 대문자로 바꾸기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
+++ 프로그래머스/unrated/181877. 대문자로 바꾸기/대문자로 바꾸기.js
@@ -1,1 +1,3 @@
 const solution = (myString) => [...myString].reduce((acc, cur) => acc + cur.toUpperCase(), '')
+
+// const solution = (myString) => [...myString].map(str => str.toUpperCase()).join('');
