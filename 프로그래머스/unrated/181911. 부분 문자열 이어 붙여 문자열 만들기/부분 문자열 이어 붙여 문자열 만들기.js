21910fb638e0245b9b80f24a3dafe6b3ed262cb6
Gangmin Lee <rkdals0301@naver.com>
2023-12-20 21:08:46 +0900

    [level unrated] Title: 부분 문자열 이어 붙여 문자열 만들기, Time: 0.47 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181911. 부분 문자열 이어 붙여 문자열 만들기/부분 문자열 이어 붙여 문자열 만들기.js
@@ -0,0 +1,8 @@
+const solution = (my_strings, parts) => {
+    let answer = '';
+    for (let i = 0; i < my_strings.length; i++) {
+        answer += [...my_strings[i]].slice(parts[i][0], parts[i][1] + 1).join('');
+    }
+    
+    return answer;
+}
