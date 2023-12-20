71c82bbcb01b1fbc006d6197bc5e15e259d8470c
Gangmin Lee <rkdals0301@naver.com>
2023-12-21 01:14:49 +0900

    [level unrated] Title: 배열 비교하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
+++ 프로그래머스/unrated/181856. 배열 비교하기/배열 비교하기.js
@@ -14,4 +14,17 @@ const solution = (arr1, arr2) => {
         else if (sum1 === sum2)
             return 0;
     }
-}
+};
+
+
+// const solution = (arr1, arr2) => {
+//     return arr1.length !== arr2.length ? compare(arr1.length, arr2.length) : compare(arr1, arr2, "reduce");
+// };
+
+// const compare = (a, b, option) => {
+//     if (option === "reduce") {
+//         a = a.reduce((acc, cur) => acc + cur);
+//         b = b.reduce((acc, cur) => acc + cur);
+//     }
+//     return a > b ? 1 : a < b ? -1 : 0;
+// };
