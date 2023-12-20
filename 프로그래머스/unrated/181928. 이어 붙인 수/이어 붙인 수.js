12383ad1a282aa82e8eb59d2fcfeadc8eb37a866
Gangmin Lee <rkdals0301@naver.com>
2023-12-21 00:59:08 +0900

    [level unrated] Title: 이어 붙인 수, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.js
@@ -5,5 +5,18 @@ const solution = (num_list) => {
         num % 2 === 0 ? evenSum += num : oddSum += num
     });
     
-    return Number(oddSum) + Number(evenSum)
+    return Number(oddSum) + Number(evenSum);
 }
+
+// const solution = (num_list) => {
+//     const { odds, evens } = num_list.reduce(({ odds, evens }, num) => {
+//         if (num % 2 === 0) {
+//             evens.push(num)
+//         } else {
+//             odds.push(num)
+//         }
+//         return { odds, evens }
+//     }, { odds: [], evens: [] });
+    
+//     return Number(odds.join('')) + Number(evens.join(''));
+// }
