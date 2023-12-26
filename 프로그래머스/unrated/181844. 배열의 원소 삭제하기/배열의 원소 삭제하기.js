e678776d18cd430b41352a98ff0492c8abaabc31
Gangmin Lee <rkdals0301@naver.com>
2023-12-27 01:41:44 +0900

    [level unrated] Title: 배열의 원소 삭제하기, Time: 0.07 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
+++ 프로그래머스/unrated/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.js
@@ -1,1 +1,4 @@
 const solution = (arr, delete_list) => arr.filter((item) => !delete_list.find((deleteItem) => deleteItem === item))
+
+
+// const solution = (arr, delete_list) => arr.filter((item) => !delete_list.includes(item))
