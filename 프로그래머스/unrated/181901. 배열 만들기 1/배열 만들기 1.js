8a635a446e08d7155825d2d473e3e26a61039314
Gangmin Lee <rkdals0301@naver.com>
2023-12-28 01:33:14 +0900

    [level unrated] Title: 배열 만들기 1, Time: 9.92 ms, Memory: 36.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.js
@@ -1,1 +1,3 @@
-const solution = (n, k) => Array.from({length: n}, (_, i) => i + 1).filter((num) => num % k === 0) 
+const solution = (n, k) => Array.from({length: n}, (_, i) => i + 1).filter((num) => num % k === 0)
+
+// const solution = (n, k) => Array(~~(n/k)).fill(k).map((v, i) => v * (i + 1))
