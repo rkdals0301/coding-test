35baa4404bb4a475ca2d940f6518a70bb6a77ede
Gangmin Lee <rkdals0301@naver.com>
2023-12-28 01:37:39 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,10 @@
+const solution = (a, b) => {
+    const answer1 = Number(`${a}${b}`)
+    const answer2 = Number(`${b}${a}`)
+    if (answer1 > answer2 || answer1 === answer2)
+        return answer1
+    else if (answer2 > answer1)
+        return answer2 
+    else 
+        return ''
+}
