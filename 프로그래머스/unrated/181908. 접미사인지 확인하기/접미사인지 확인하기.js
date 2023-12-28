04016789ead16ba069b732a6d88957c8b4854ad5
Gangmin Lee <rkdals0301@naver.com>
2023-12-29 02:20:40 +0900

    [level unrated] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
+++ 프로그래머스/unrated/181908. 접미사인지 확인하기/접미사인지 확인하기.js
@@ -1,1 +1,3 @@
 const solution = (my_string, is_suffix) => my_string.slice(my_string.length - is_suffix.length) === is_suffix ? 1 : 0
+
+// const solution = (my_string, is_suffix) => my_string.endsWith(is_suffix) ? 1 : 0
