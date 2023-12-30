f849ab2a08742265eff932dfda5efe3bf9402abb
Gangmin Lee <rkdals0301@naver.com>
2023-12-30 18:52:52 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,1 +1,12 @@
 const solution = (num_list) => num_list.reduce((acc, cur) => acc * cur, 1) < num_list.reduce((acc, cur) => acc + cur, 0) ** 2 ? 1 : 0
+
+
+// const solution = (num_list) => {
+//     let accMul = 1
+//     let accSum = 0
+//     for (const num of num_list) {
+//         accMul *= num
+//         accSum += num
+//     }
+//     return accMul < accSum ** 2 ? 1 : 0
+// }
