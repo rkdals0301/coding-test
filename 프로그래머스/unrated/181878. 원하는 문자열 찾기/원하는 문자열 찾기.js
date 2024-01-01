6bed11daeda7973ca6943dce9e19e4470d618228
Gangmin Lee <rkdals0301@naver.com>
2024-01-01 23:05:29 +0900

    [level unrated] Title: 원하는 문자열 찾기, Time: 0.15 ms, Memory: 33.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
+++ 프로그래머스/unrated/181878. 원하는 문자열 찾기/원하는 문자열 찾기.js
@@ -1,1 +1,3 @@
 const solution = (myString, pat) => +myString.toLowerCase().includes(pat.toLowerCase())
+
+// const solution = (myString, pat) => myString.toUpperCase().includes(pat.toUpperCase()) ? 1 : 0 
