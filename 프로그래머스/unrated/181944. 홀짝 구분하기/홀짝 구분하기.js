ad4728f887571cc0c430994f748587844467f2ce
Gangmin Lee <rkdals0301@naver.com>
2024-01-05 00:09:57 +0900

    [level unrated] Title: 홀짝 구분하기, Time: 46.42 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
+++ 프로그래머스/unrated/181944. 홀짝 구분하기/홀짝 구분하기.js
@@ -12,3 +12,13 @@ rl.on('line', function (line) {
     n = Number(input[0]);
     console.log(n % 2 === 0 ? `${n} is even` : `${n} is odd`)
 });
+
+
+// const readline = require('readline');
+// const rl = readline.createInterface({
+//     input: process.stdin,
+//     output: process.stdout
+// }).on('line', function (line) {
+//     const result = Number(line) % 2 ? 'odd' : 'even'
+//     console.log(line, 'is', result)
+// })
