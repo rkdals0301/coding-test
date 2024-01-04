5ef3fe4ea92e09420472a7b1e9cd10976d079c77
Gangmin Lee <rkdals0301@naver.com>
2024-01-04 21:35:35 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -5,3 +5,8 @@ const solution = (num_list) => {
     
     return num_list
 }
+
+// const solution = (num_list) => {
+//     const [a, b] = [...num_list].reverse();
+//     return [...num_list, a > b ? (a - b) : a * 2];
+// }
