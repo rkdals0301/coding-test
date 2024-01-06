bb26b2310e6839eb85ba448c25f9fa4f2948d8a8
Gangmin Lee <rkdals0301@naver.com>
2024-01-06 23:19:27 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,4 +1,1 @@
-const solution = (num_list) => {
-    const index = num_list.findIndex((num, index) => num < 0)
-    return index !== undefined ? index : -1 
-}
+const solution = (num_list) => num_list.findIndex((num, index) => num < 0)
