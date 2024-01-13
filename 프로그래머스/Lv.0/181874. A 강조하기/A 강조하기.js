0f0a1f1d1e75403f8dfd8c54535fb60d32407931
Gangmin Lee <rkdals0301@naver.com>
2024-01-13 22:12:21 +0900

    [level Lv.0] Title: A 강조하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
+++ 프로그래머스/Lv.0/181874. A 강조하기/A 강조하기.js
@@ -1,1 +1,3 @@
-const solution = (myString) => [...myString].map((string) => string.toLowerCase() === 'a' ? string.toUpperCase() : string.toLowerCase()).join('')
+// const solution = (myString) => [...myString].map((string) => string.toLowerCase() === 'a' ? string.toUpperCase() : string.toLowerCase()).join('')
+
+const solution = (myString) => myString.toLowerCase().replaceAll('a', 'A');
