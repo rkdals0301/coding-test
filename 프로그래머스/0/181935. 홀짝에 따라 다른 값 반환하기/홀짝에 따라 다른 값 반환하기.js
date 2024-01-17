8dad0c3f55240b3b96b0581a143589f7f1869178
Gangmin Lee <rkdals0301@naver.com>
2024-01-17 22:18:02 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.17 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -10,3 +10,10 @@ const solution = (n) => {
     
     return answer
 }
+
+// const solution = (n) => {
+//     if(n%2===1)
+//       return  (n+1)/2*((n + 1)/2);
+//     else
+//       return   n*(n+1)*(n+2)/6;
+// }
