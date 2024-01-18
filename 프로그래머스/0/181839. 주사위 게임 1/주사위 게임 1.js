36617c9abd6584576e543dc52c77ca0579a2e7c3
Gangmin Lee <rkdals0301@naver.com>
2024-01-18 22:56:52 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.js
@@ -0,0 +1,12 @@
+const solution = (a, b) => {
+    let answer = 0
+    if (a % 2 !== 0 && b % 2 !== 0) {
+        answer = (a * a) + (b * b)
+    } else if (a % 2 !== 0 || b % 2 !== 0) {
+        answer = 2 * (a + b)
+    } else if (a % 2 === 0 && b % 2 === 0) {
+        answer = Math.abs(a - b)
+    }
+    
+    return answer
+}
