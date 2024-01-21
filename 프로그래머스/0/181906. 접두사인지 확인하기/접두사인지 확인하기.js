e89c1626b55d1512978f91ba79e323298f9c9a22
Gangmin Lee <rkdals0301@naver.com>
2024-01-21 19:44:22 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.js
@@ -1,1 +1,3 @@
 const solution = (my_string, is_prefix) => +my_string.startsWith(is_prefix)
+
+// const solution = (my_string, is_prefix) => my_string.slice(0, is_prefix.length) === is_prefix ? 1 : 0
