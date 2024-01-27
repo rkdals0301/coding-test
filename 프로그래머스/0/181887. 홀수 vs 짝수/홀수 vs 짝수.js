75154c91e21f5f5e1d78a7e1dc93d07b93ebbacf
Gangmin Lee <rkdals0301@naver.com>
2024-01-27 20:49:00 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.js
@@ -10,3 +10,16 @@ const solution = (num_list) => {
     
     return Math.max(odd, even)
 }
+
+
+
+// const solution = (num_list) => {
+//     let even = 0;
+//     let odd = 0
+
+//     num_list.map((v, idx) => {
+//         !(idx % 2) ? even += v : odd += v; 
+//     })
+
+//     return odd > even ? odd : even; 
+// }
