811c9db595150e373194c8aff5de5c17b1c90e26
Gangmin Lee <rkdals0301@naver.com>
2024-01-28 23:12:39 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.js
@@ -0,0 +1,12 @@
+const solution = (myString, pat) => {
+    const string = [...myString].map((string) => {
+        if (string === 'A')
+            return string.replaceAll('A', 'B')
+        else if (string === 'B')
+            return string.replaceAll('B', 'A')
+        
+        return string
+    }).join('')
+    
+    return string.includes(pat) ? 1 : 0
+}
