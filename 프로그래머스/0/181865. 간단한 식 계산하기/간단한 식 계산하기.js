73eb73cc071d67bc8587706b76dce4051c595210
Gangmin Lee <rkdals0301@naver.com>
2024-02-03 17:22:50 +0900

    [level 0] Title: 간단한 식 계산하기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,7 @@
+const solution = (binomial) => {
+    const [a, op, b] = binomial.split(' ');
+    
+    if(op === '+') return Number(a) + Number(b);
+    if(op === '-') return Number(a) - Number(b);
+    if(op === '*') return Number(a) * Number(b);   
+}
