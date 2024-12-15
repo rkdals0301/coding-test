0996e48ecf223e692182cc02846458f2a11e24a4
Gangmin Lee <rkdals0301@naver.com>
2024-12-15 23:26:03 +0900

    [level 0] Title: 각도기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120829. 각도기/각도기.js
+++ 프로그래머스/0/120829. 각도기/각도기.js
@@ -1,11 +1,2 @@
-const solution = angle => {
-    if (angle < 90 && angle > 0) {
-        return 1
-    } else if (angle === 90) {
-        return 2
-    } else if (angle > 90 && angle < 180) {
-        return 3
-    } else if (angle === 180) {
-        return 4
-    }
-}
+const solution = angle => [0, 90, 91, 180].filter(x => angle >= x).length;
+
